f4baa5a3fdf4fd9b3499f90105799d8013d911b6
figue0122 <you@example.com>
2024-02-02 22:38:22 -0400

    fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
     return;
   }
-
   fs.writeFile(filePath, body, 'utf8', (err) => {
     if (err) {
-      console.error(err);
+      console.log(err);
     }
   });
 });
